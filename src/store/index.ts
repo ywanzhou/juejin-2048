0ecda8d129955ad3fcbfc995c9ac5527336d932d
Zhou-Zhonghua <pacpc@qq.com>
2022-03-29 08:30:16 +0800

    build: build

--- src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { createPinia } from 'pinia'
-import { useCountStore } from './modules/count'
-const store = createPinia()
-
-export default store
-export { useCountStore }
--- src/store/modules/types.ts
+++
@@ -1,3 +0,0 @@
-export interface CountInterface {
-  count: number
-}
